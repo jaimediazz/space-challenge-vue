be8b6fe7334503986c382032a939b806482a182c
Jaime <jaime99dr@gmail.com>
2021-12-14 16:33:06 +0000

    push a github

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container d-flex flex-column">
+    <!-- HEADER -->
+    <app-header></app-header>
+    <div class="row mt-5 d-flex align-items-center">
+      <!-- MAIN TEXT -->
+      <div class="col">
+        <div class="text-format">
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!</p>
+        </div>
+      </div>
+      <!-- EXPLORE BUTTON -->
+      <div class="col d-flex justify-content-end">
+        <div class="card border border-1 rounded-circle">
+          <div class="card-img-overlay d-flex justify-content-center align-items-center">
+            <h4> <a href="./destinations/destination-moon.html"> EXPLORE </a> </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './header/Header.vue';
+
+export default {
+/*     data() {
+        return {
+            : 
+        }
+    }, */
+    components: {
+        appHeader: Header
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row mb-5">
+      <div class="col-2 mt-5">
+        <img :src="logo" alt="logo">
+      </div>
+      <div class="col-10 mt-5 d-flex justify-content-end align-items-center" 
+        style="background-image: url('~/assets/home/Rectangle.png');">
+        <ul class="nav" id="nav-bar">
+          <li class="nav-item active"> 
+            <router-link to="/"> 00 Home </router-link>
+          </li>
+          <li class="nav-item"> 
+            <a href="./destinations/destination-moon.html"> 01 Destination </a> 
+          </li>
+          <li class="nav-item"> 
+            <a href="./crew-members/crew-commander.html"> 02 Crew </a>  
+          </li>
+          <li class="nav-item"> 
+            <a href="./technologies/technology-vehicle.html"> 03 Technology </a>  
+          </li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            logo: '../../assets/shared/logo.svg'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
