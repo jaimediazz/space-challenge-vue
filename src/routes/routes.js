85de651d40aaea40ce9d4f9b72f70ba9aaed564f
Jaime <jaime99dr@gmail.com>
2021-12-15 14:48:56 +0000

    route.js error fixed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,4 @@
 import Home from '../components/Home.vue';
-import Moon from '../components/destinations/Moon.vue';
-/* import Mars from '../components/destinations/Moon.vue';
-import Moon from '../components/destinations/Moon.vue';
-import Moon from '../components/destinations/Moon.vue'; */
 import Destination from '../components/Destination.vue';
 
 export const routes = [
