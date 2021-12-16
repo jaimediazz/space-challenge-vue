7c57b28188ad94e8badbb578e5de81ddade1ce53
Jaime <jaime99dr@gmail.com>
2021-12-16 10:56:38 +0000

    technology main header

--- src/components/Technology.vue
+++ src/components/Technology.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <h1>holaaaa</h1>    
+    <div class="container d-flex flex-column">
+        <app-header></app-header>    
     </div>    
 </template>
 
 <script>
+import Header from './header/Header.vue';
+
 export default {
-    
+    components: {
+        appHeader: Header
+    }    
 }
 </script>
 
