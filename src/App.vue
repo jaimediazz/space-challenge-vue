189113c71a5eeff3b8b53e3c0cd29d382ed44560
Jaime <jaime99dr@gmail.com>
2021-12-16 13:42:58 +0000

    change background image done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="bg-image" style="background-image: url('./assets/home/background-home-desktop.jpg'); height: 100vh;">
+  <div class="bg-image" :style="{ backgroundImage: 'url(' + backgroundImg + ')', height: '100vh'}">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { eventBus } from './main';
 import Home from './components/Home.vue';
 
 export default {
+  data() {
+    return {
+      backgroundImg: '../assets/home/background-home-desktop.jpg',
+/*       backgroundImgRoute: {
+        home: '../assets/home/background-home-desktop.jpg',
+        destination: '../assets/destination/background-destination-desktop.jpg',
+        crew: '../assets/crew/background-crew-desktop.jpg',
+        technology: '../assets/technology/background-technology-desktop.jpg'
+      } */
+    }
+  },
   components: {
     appHome: Home
+  },
+  methods: {
+    getBackgroundImageRoute() {
+      return this.backgroundImg;
+    }
+  },
+  created() {
+    eventBus.$on('tabWasChanged', (backgroundImg) => {
+      this.backgroundImg = backgroundImg;   
+    })
   }
 }
 </script>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -7,16 +7,16 @@
         style="background-image: url('~/assets/home/Rectangle.png');">
         <ul class="nav" id="nav-bar">
           <li class="nav-item active"> 
-            <router-link to="/"> 00 Home </router-link>
+            <router-link to="/" @click.native="changeSelectedTab('home')"> 00 Home </router-link>
           </li>
           <li class="nav-item"> 
-            <router-link to="/destination"> 01 Destination </router-link> 
+            <router-link to="/destination" @click.native="changeSelectedTab('destination')"> 01 Destination </router-link> 
           </li>
           <li class="nav-item">
-            <router-link to="/crewMember"> 02 Crew </router-link> 
+            <router-link to="/crewMember" @click.native="changeSelectedTab('crew')"> 02 Crew </router-link> 
           </li>
           <li class="nav-item">
-            <router-link to="/technology"> 03 Technology </router-link> 
+            <router-link to="/technology" @click.native="changeSelectedTab('technology')"> 03 Technology </router-link> 
           </li>
         </ul>
       </div>
@@ -24,11 +24,33 @@
 </template>
 
 <script>
+import { eventBus } from '../../main';
+
 export default {
     data() {
         return {
-          logo: '../../assets/shared/logo.svg'
+          logo: '../../assets/shared/logo.svg',
+          backgroundImageRoute: 'home'
         }
+    },
+    methods: {
+      changeSelectedTab(tab) {
+        switch(tab) {
+          case 'home':
+            this.backgroundImageRoute ='../assets/home/background-home-desktop.jpg';
+            break;
+          case 'destination':
+            this.backgroundImageRoute ='../assets/destination/background-destination-desktop.jpg';
+            break;
+          case 'crew':
+            this.backgroundImageRoute ='../assets/crew/background-crew-desktop.jpg';
+            break;
+          case 'technology':
+            this.backgroundImageRoute ='../assets/technology/background-technology-desktop.jpg';
+            break;                                                
+        }
+        eventBus.$emit('tabWasChanged', this.backgroundImageRoute);
+      }
     }
 }
 </script>
