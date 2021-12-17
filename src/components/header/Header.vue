46fa7368e5882be26d9701d8ebd1e89194d68b2c
Jaime <jaime99dr@gmail.com>
2021-12-17 19:20:32 +0000

    fix rectangle error

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -3,20 +3,19 @@
       <div class="col-2 mt-5">
         <img :src="logo" alt="logo">
       </div>
-      <div class="col-10 mt-5 d-flex justify-content-end align-items-center" 
-        style="background-image: url('~/assets/home/Rectangle.png');">
+      <div class="col-10 mt-5 d-flex justify-content-end align-items-center">
         <ul class="nav" id="nav-bar">
-          <li class="nav-item active"> 
-            <router-link to="/" @click.native="changeSelectedTab('home')"> 00 Home </router-link>
+          <li class="nav-item" :class="{active: isActive}"> 
+            <router-link to="/" @click.native="changeSelectedTab('home')"> 00 HOME </router-link>
           </li>
-          <li class="nav-item"> 
-            <router-link to="/destination" @click.native="changeSelectedTab('destination')"> 01 Destination </router-link> 
+          <li class="nav-item" :class="{active: isActive}"> 
+            <router-link to="/destination" @click.native="changeSelectedTab('destination')"> 01 DESTINATION </router-link> 
           </li>
-          <li class="nav-item">
-            <router-link to="/crewMember" @click.native="changeSelectedTab('crew')"> 02 Crew </router-link> 
+          <li class="nav-item" :class="{active: isActive}">
+            <router-link to="/crewMember" @click.native="changeSelectedTab('crew')"> 02 CREW </router-link> 
           </li>
-          <li class="nav-item">
-            <router-link to="/technology" @click.native="changeSelectedTab('technology')"> 03 Technology </router-link> 
+          <li class="nav-item" :class="{active: isActive}">
+            <router-link to="/technology" @click.native="changeSelectedTab('technology')"> 03 TECHNOLOGY </router-link> 
           </li>
         </ul>
       </div>
